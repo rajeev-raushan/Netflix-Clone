88e2ad48e5e0e17ed63463ed9ebaa8ddc976ad68
rajeev raushan <raushanrajeev799@gmail.com>
2022-12-21 00:59:19 +0530

    added movie trailer feature

--- src/Row.js
+++ src/Row.js
@@ -1,18 +1,21 @@
 import React,{useState,useEffect} from 'react'
 import axios from './axios';
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from 'movie-trailer'
 const base_url="https://image.tmdb.org/t/p/original/";
+
 
 
 function Row({title,fetchUrl,IsLargeRow}){
     const [movies,setMovies]=useState([]);
+    const [trailerUrl,setTrailerUrl]=useState("");
     //a snippet of code which runs based on a specific condition 
     useEffect(()=>{
         //if [],run once when the row loads,and donot run again 
 
         async function fetchData(){
             const request=await axios.get(fetchUrl);
-            console.log(request.data.results);
             setMovies(request.data.results);
 
             return request;
@@ -21,11 +24,35 @@ function Row({title,fetchUrl,IsLargeRow}){
         fetchData();
 
     },[fetchUrl]);
-    console.log(movies);
+
+const opts={
+    height:"390",
+    width:"100%"
+
+}
+
+    const handleclick=(movie)=>{
+        if(trailerUrl){
+            setTrailerUrl("");
+        }else{
+            // console.log(movie.title)
+            movieTrailer(movie?.title||"")
+            
+            .then((url)=>{
+                // console.log(url);
+                const urlParams= new URLSearchParams(new URL(url).search);
+                // console.log(urlParams);
+            setTrailerUrl(urlParams.get('v'));
+
+            })
+            .catch((error)=>console.log(error))
+        }
+    }
 
     return(
        
         <div className='row'>
+        
          <h1>{title}</h1>
          <div className="row_posters">
             {/* { several row_poster} */}
@@ -34,6 +61,7 @@ function Row({title,fetchUrl,IsLargeRow}){
             {movies.map((movie) => (
             <img 
               key={movie.id}
+              onClick={()=>handleclick(movie)}
               className={`row_poster ${IsLargeRow && "row_LargerPoster"}`}
                 src={`${base_url}${
                     IsLargeRow?  movie.poster_path :movie.backdrop_path
@@ -43,6 +71,7 @@ function Row({title,fetchUrl,IsLargeRow}){
             
             ))}
          </div>
+         {trailerUrl&&<YouTube videoId={trailerUrl} opts={opts}/>}
 
         </div>
     )
